301019c945c64e9f4ebbacc24061b8c137f50af1
Andhika <93108100+AndhikaAmarullah@users.noreply.github.com>
2023-06-16 18:42:05 +0700

    Update app.py

--- API/app.py
+++ API/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from Routes.predictRoute import predict_route
+from Routes.animalRoute import animal_route
 
 app = Flask(__name__)
 
 app.register_blueprint(predict_route)
+app.register_blueprint(animal_route)
 
 if __name__ == '__main__':
     app.run()
